cd6c6af058cf58410c356812fb2b1af1f61f537c
remarshal <marshalwaldendev@gmail.com>
2022-02-24 13:49:37 -0500

    task 1 - rev 2

--- week-1.js
+++ week-1.js
@@ -1,11 +1,15 @@
-function Student(name, email, community) {
-    this.name = name;
-    this.email = email;
-    this.community = community;
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
 }
 
-function Bootcamp(name, level, students[]) {
-    this.name = name;
-    this.level = level;
-    this.students = students;
+class Bootcamp {
+    constructor(name, level, students[]) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
 }
