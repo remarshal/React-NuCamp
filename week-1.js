577aaeafbfd9e97453c1c428f5a7a2a0d82921af
remarshal <marshalwaldendev@gmail.com>
2022-02-24 13:38:49 -0500

    task 1

---
+++ week-1.js
@@ -0,0 +1,11 @@
+function Student(name, email, community) {
+    this.name = name;
+    this.email = email;
+    this.community = community;
+}
+
+function Bootcamp(name, level, students[]) {
+    this.name = name;
+    this.level = level;
+    this.students = students;
+}
