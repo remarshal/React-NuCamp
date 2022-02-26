6faa6e0beb4479c02c8fd5dd5ce357d170e59931
remarshal <marshalwaldendev@gmail.com>
2022-02-26 15:50:25 -0500

    updated code to less

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -13,18 +13,14 @@ class Bootcamp {
         this.students = students;
     }
     registerStudent(newStudent) {
-        // check if the new student's email already existings in current students
-        const duplicateStudentsFound = this.students.filter((existingStudent) => {
-            if (existingStudent.email === newStudent.email) {
-                return true
-            }
-        })
+        // check if the new students email already existings in current students
+        const duplicateStudentsFound = this.students.filter(existingStudent => existingStudent.email === newStudent.email)
         // if student not registered, then add to array of students
         if (duplicateStudentsFound.length === 0) {
             this.students.push(newStudent)
             console.log(`Thank you for registering ${newStudent.email} to the bootcamp ${this.name}.`)
             return this.students;
-        // else show message that studen tis already registered 
+        // else show message that student is already registered 
         } else {
             console.log(`The students email ${newStudent.email} is already registered to the bootcamp ${this.name}.`)
             return this.students;
