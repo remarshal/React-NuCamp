dec38b446cbd9cb9e0a9d97d79674cdd5944f834
remarshal <marshalwaldendev@gmail.com>
2022-03-06 10:30:23 -0500

    after class

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfoComponent from './CampsiteInfoComponent';
 
 class Directory extends Component {
     constructor(props) {
@@ -10,22 +11,7 @@ class Directory extends Component {
     }
 
     onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
+        this.setState({ selectedCampsite: campsite });
     }
 
     render() {
@@ -47,11 +33,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfoComponent campsite={this.state.selectedCampsite} />
             </div>
         );
     }
